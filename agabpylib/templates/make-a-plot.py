314360ebfdb4e5dc190d9daabbb826de8c5464c9
agabrown <agabrown@xs4all.nl>
2018-01-21 16:50:14 +0100

    python code templates folder added

---
+++ agabpylib/templates/make-a-plot.py
@@ -0,0 +1,42 @@
+"""
+Template for a simple script for making plots.
+
+Anthony Brown Jan 2018
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+from agabpylib.plotting.plotstyles import useagab, apply_tufte
+from agabpylib.plotting.distinct_colours import get_distinct
+
+def make_plot(args):
+    """
+    Code to make the plot.
+
+    Parameters
+    ----------
+
+    args : dictionary
+        Command line arguments
+
+    Returns
+    -------
+
+    Nothing
+    """
+
+def parseCommandLineArguments():
+    """
+    Set up command line parsing.
+    """
+    parser = argparse.ArgumentParser(description="""Describe the plot.""")
+    parser.add_argument("-p", action="store_true", dest="pdfOutput", help="Make PDF plot")
+    parser.add_argument("-b", action="store_true", dest="pngOutput", help="Make PNG plot")
+    args=vars(parser.parse_args())
+    return args
+
+if __name__ in ('__main__'):
+    args=parseCommandLineArguments()
+    make_plot(args)
