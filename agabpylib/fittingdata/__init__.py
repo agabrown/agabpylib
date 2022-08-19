a7fb7979c7ba4b00e247c0a30d115065defcde6a
agabrown <agabrown@xs4all.nl>
2022-08-19 13:00:13 +0200

    fitting data removed

--- agabpylib/fittingdata/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Tools for fitting models to data.
-
-This package provides functions fitting models to data.
-"""
-
-from .fitpolynomials import *
-
-__all__ = [s for s in dir() if not s.startswith("_")]
